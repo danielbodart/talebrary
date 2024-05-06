fec459a2dd6c43bd7f5c22ad3a06b06aac50c80f
Daniel Bodart <dan@bodar.com>
2024-05-06 12:48:31 +0100

    Removed index.html mapping as it might not be needed

--- src/http.ts
+++ src/http.ts
@@ -17,3 +17,43 @@ export function request(method: HttpMethod, uri: string, headers: HttpHeader[]):
 export function get(uri: string, ...headers: HttpHeader[]): Request {
     return request('GET', uri, headers);
 }
+
+/**
+ * Uri class based on {@link https://tools.ietf.org/html/rfc3986 RFC 3986}
+ */
+export class Uri {
+    /** {@link https://tools.ietf.org/html/rfc3986#appendix-B } */
+    static RFC_3986 = /^(([^:/?#]+):)?(\/\/([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?/;
+    scheme?: string;
+    authority?: string;
+    path: string;
+    query?: string;
+    fragment?: string;
+
+    constructor(value: string) {
+        const match = Uri.RFC_3986.exec(value);
+        if (!match) throw new Error(`Invalid Uri: ${value}`);
+        const [, , scheme, , authority, path, , query, , fragment] = match;
+        this.scheme = scheme;
+        this.authority = authority;
+        this.path = path;
+        this.query = query;
+        this.fragment = fragment;
+    }
+
+    /** {@link https://tools.ietf.org/html/rfc3986#section-5.3} */
+    toString() {
+        const result: string[] = [];
+
+        if (typeof this.scheme != 'undefined') result.push(this.scheme, ":");
+        if (typeof this.authority != 'undefined') result.push("//", this.authority);
+        result.push(this.path);
+        if (typeof this.query != 'undefined') result.push("?", this.query);
+        if (typeof this.fragment != 'undefined') result.push("#", this.fragment);
+        return result.join('');
+    }
+
+    toJSON(){
+        return this.toString();
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import {client} from "./http.ts";
 
 export default {
     fetch(request: Request, env: any) {
-        if (request.url === '/') {
-            return client(new Request('/index.html', request));
-        }
         return client(request);
-    },
+    }
 }
