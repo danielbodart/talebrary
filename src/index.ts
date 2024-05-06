89b88db7f36aa129510bb37ba5b4de2e52fe020c
Daniel Bodart <dan@bodar.com>
2024-05-06 14:32:34 +0100

    Add support for subdirectories with index files

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@ import {client, Uri} from "./http.ts";
 export default {
     fetch(request: Request, env: any) {
         const uri = new Uri(request.url);
-        if (uri.path === '/') {
-            uri.path = '/index.html';
+        if (uri.path.endsWith('/')) {
+            uri.path += '/index.html';
             return client(new Request(uri.toString(), request));
         }
         return client(request);
