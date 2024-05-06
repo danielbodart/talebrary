cc19887e7af1da5c44b55cf638755738f665c1a0
Daniel Bodart <dan@bodar.com>
2024-05-06 12:56:43 +0100

    Add back index.html support

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
-import {client} from "./http.ts";
+import {client, Uri} from "./http.ts";
 
 export default {
     fetch(request: Request, env: any) {
+        const uri = new Uri(request.url);
+        if (uri.path === '/') {
+            uri.path = '/index.html';
+            return client(new Request(uri.toString(), request));
+        }
         return client(request);
-    }
+    },
 }
