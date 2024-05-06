6840ea91fdb6f0370c9a6c5ca52e42fcf184f8cb
Daniel Bodart <dan@bodar.com>
2024-05-06 18:26:40 +0100

    Fix index.html

--- src/App.ts
+++ src/App.ts
@@ -7,7 +7,7 @@ export class App {
     handle(request: Request): Promise<Response> {
         const uri = new Uri(request.url);
         if (uri.path.endsWith('/')) {
-            uri.path += '/index.html';
+            uri.path += 'index.html';
             return this.handler(new Request(uri.toString(), request));
         }
         return this.handler(request);
