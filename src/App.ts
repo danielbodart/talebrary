8a0ba2e63fc8639630926fb979c4fd139eec9127
Daniel Bodart <dan@bodar.com>
2024-05-07 17:42:58 +0100

    Last try with Deno

--- src/App.ts
+++ src/App.ts
@@ -1,13 +1,14 @@
 import {HttpHandler, Uri} from "./http.ts";
+import { D1Database } from "npm:@cloudflare/workers-types";
 
 export class App {
-    constructor(private handler: HttpHandler, private env: any) {
+    constructor(private handler: HttpHandler, private db: D1Database) {
     }
 
     async handle(request: Request): Promise<Response> {
         const uri = new Uri(request.url);
         if (uri.path === '/env') {
-            return new Response(JSON.stringify(this.env))
+            return new Response(JSON.stringify(this.db))
         }
 
         if (uri.path.endsWith('/')) {
