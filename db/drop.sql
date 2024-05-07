217253c401a45385a33617d85dbee28b7543bfab
Daniel Bodart <dan@bodar.com>
2024-05-07 09:44:45 +0100

    Add database dump

---
+++ db/drop.sql
@@ -0,0 +1,81 @@
+drop table clubmembers;
+
+drop table clubs;
+
+drop table compdivs;
+
+drop table competitions;
+
+drop table compgames;
+
+drop table compprofilelinks;
+
+drop table comps_history;
+
+drop table crossrecs;
+
+drop table downloadhelp;
+
+drop table extreviews;
+
+drop table filetypes;
+
+drop table gamefwds;
+
+drop table gameprofilelinks;
+
+drop table games_history;
+
+drop table gametags;
+
+drop table gamexrefs;
+
+drop table gamexreftypes;
+
+drop table ifids;
+
+drop table iso639;
+
+drop table iso639x;
+
+drop table mirrors;
+
+drop table news;
+
+drop table operatingsystems;
+
+drop table osversions;
+
+drop table playedgames;
+
+drop table pollcomments;
+
+drop table polls;
+
+drop table pollvotes;
+
+drop table reclistitems;
+
+drop table reclists;
+
+drop table reviewtags;
+
+drop table reviewvotes;
+
+drop table sitenews;
+
+drop table specialreviewers;
+
+drop table stylesheets;
+
+drop table tagstats;
+
+drop table ucomments;
+
+drop table unwishlists;
+
+drop table users;
+
+drop table wishlists;
+
+vacuum;
