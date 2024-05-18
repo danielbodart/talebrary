e37c6e7719b48fdce71b58914c1aa8709c7f2fa8
Daniel Bodart <dan@bodar.com>
2024-05-18 23:06:28 +0100

    Got basic template decoration working

---
+++ src/ApplicationScope.ts
@@ -0,0 +1,30 @@
+import {D1Database} from "@cloudflare/workers-types";
+import {Librarian} from "./Librarian.tsx";
+import {D1GameFinder} from "./D1GameFinder.ts";
+import {type HttpHandler} from "./http.ts";
+import {Routing} from "./Routing.ts";
+import {htmlHandler} from "./HtmlHandler.tsx";
+
+export interface Env {
+    db: D1Database;
+}
+
+export class ScopeBuilder {
+    add<R = object>(fun: (scope: this) => R): this & R ;
+    add<R = object>(instance: R): this & R ;
+    add<R = object>(value: any): this & R {
+        const result = typeof value === 'function' ? value(this) : value;
+        return Object.assign(this, result);
+    }
+}
+
+export function applicationScope(db: D1Database, httpClient: HttpHandler) {
+    return new ScopeBuilder()
+        .add({db, httpClient})
+        .add(({db}) => ({finder: new D1GameFinder(db)}))
+        .add(({finder}) => ({librarian: new Librarian(finder)}))
+        .add(({httpClient, librarian}) => ({routing: new Routing(httpClient, librarian)}))
+        .add(({routing}) => ({handler: htmlHandler(request => routing.handle(request))}))
+}
+
+export type ApplicationScope = Omit<ReturnType<typeof applicationScope>, keyof ScopeBuilder>
---
+++ src/HtmlHandler.tsx
@@ -0,0 +1,44 @@
+import {type HttpHandler, Uri} from "./http.ts";
+import {parseHTML} from 'linkedom';
+import * as elements from 'typed-html';
+
+const htmlTypes = new Set(["text/html"]);
+
+export function htmlHandler(http: HttpHandler): HttpHandler {
+    return async (request) => {
+        const response = await http(request);
+        if (!response.ok) return response;
+
+        const contentType = response.headers.get('content-type');
+        if ((contentType && htmlTypes.has(contentType)) || new Uri(request.url).path.endsWith('.html')) {
+            const {document} = parseHTML(await response.text());
+            return new Response(template(document), response)
+        }
+
+        return response;
+    }
+}
+
+export function template(document: Document) {
+    return '<!DOCTYPE html>' +
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8"/>
+            <title>{document.title}</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com"/>
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="crossorigin"/>
+            <link
+                href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@700&family=Libre+Baskerville:ital,wght@0,400;0,700;1,400&display=swap"
+                rel="stylesheet"/>
+            <link rel="stylesheet" href="/style.css"/>
+        </head>
+        <body>
+        <header>
+            <h1>{document.title}</h1>
+        </header>
+        <main>
+            {document.body}
+        </main>
+        </body>
+        </html>;
+}
