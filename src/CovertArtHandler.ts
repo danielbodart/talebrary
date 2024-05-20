1375edf58353aad45ee41ae08633b2012dd310e3
Daniel Bodart <dan@bodar.com>
2024-05-20 20:25:36 +0100

    Does this work in prod

--- src/CovertArtHandler.ts
+++ src/CovertArtHandler.ts
@@ -5,36 +5,41 @@ import {MediaType} from "./MediaType.ts";
 
 export function covertArtHandler(http: HttpHandler, r2: R2Bucket): HttpHandler {
     return async (request) => {
-        const {path} = new Uri(request.url);
-        const response = await http(request);
-        if (!path.endsWith('/cover-art')) {
-            return response;
-        }
+        const uri = new Uri(request.url);
+        const {path} = uri;
+        if (path.endsWith('/cover-art')) {
+            // Encode slashes as that is what R2 expects!
+            uri.path = `/${encodeURIComponent(uri.path)}`;
+            const newUri = uri.toString();
+            console.log('newUri', newUri);
+            const response = await http(new Request(newUri, request));
+            if (response.status === 404) {
+                console.log('404', newUri);
+                const [, , id] = path.split('/');
 
-        if (response.status === 404) {
-            const [, , id] = path.split('/');
+                const response = await http(get(`https://ifdb.org/viewgame?coverart&id=${id}`));
+                if (!response.ok) return response;
 
-            const response = await http(get(`https://ifdb.org/viewgame?coverart&id=${id}`));
-            if (!response.ok) return response;
-
-            const body = response.body;
-            if (body) {
-                const [one, two] = body.tee();
-                try {
-                    await r2.put(path, one as any, {
-                        httpMetadata: {
-                            contentType: response.headers.get('content-type') ?? MediaType.APPLICATION_OCTET_STREAM,
-                            cacheControl: 'public, max-age=60'
-                        }
-                    });
-                } catch (e) {
-                    console.error(e);
+                const body = response.body;
+                if (body) {
+                    const [one, two] = body.tee();
+                    try {
+                        const result = await r2.put(path, one as any, {
+                            httpMetadata: {
+                                contentType: response.headers.get('content-type') ?? MediaType.APPLICATION_OCTET_STREAM,
+                                cacheControl: 'public, max-age=60'
+                            }
+                        });
+                        console.log(result);
+                    } catch (e) {
+                        console.error(e);
+                    }
+                    return new Response(two, {status: 200})
                 }
-                return new Response(two, {status: 200})
             }
         }
 
-        return response;
+        return http(request);
     }
 }
 
