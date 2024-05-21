cfb473e84bee698140f3733c2e7c61f2968ba9af
Daniel Bodart <dan@bodar.com>
2024-05-21 16:11:48 +0100

    Add Etag support

---
+++ src/EtagHandler.ts
@@ -0,0 +1,32 @@
+import type {HttpHandler} from "./http.ts";
+
+const safeHeaders = [
+    'date',
+    'last-modified',
+    'cache-control',
+    'expires',
+    'set-cookie'
+];
+
+function copySafeHeaders(source: Response): Record<string, string> {
+    return safeHeaders.reduce((a, header) => {
+        const value = source.headers.get(header);
+        if (value) a[header] = value;
+        return a;
+    }, {} as Record<string, string>);
+}
+
+export function etagHandler(http: HttpHandler) {
+    return async (request: Request) => {
+        const response = await http(request);
+        const etag = response.headers.get('etag');
+        if (!(request.method === 'GET' && response.ok && !etag)) return response;
+
+        if (etag === request.headers.get('if-none-match')) {
+            return new Response(null, {status: 304, headers: copySafeHeaders(response)});
+        }
+        response.headers.set('etag', etag!);
+        return response;
+    }
+}
+
