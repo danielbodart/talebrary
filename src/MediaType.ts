35415e75f2263ddede66d4aa98648193c3b47d8e
Daniel Bodart <dan@bodar.com>
2024-05-23 07:28:24 +0100

    Switch to using asdf rclone plugin, get rclone content pull working, remove uneeded content type code

--- src/MediaType.ts
+++ src/MediaType.ts
@@ -1,28 +1,2 @@
 export class MediaType {
-    static WILDCARD = "*/*";
-    static APPLICATION_XML = "application/xml";
-    static APPLICATION_ATOM_XML = "application/atom+xml";
-    static APPLICATION_XHTML_XML = "application/xhtml+xml";
-    static APPLICATION_SVG_XML = "application/svg+xml";
-    static APPLICATION_JAVASCRIPT = "application/javascript";
-    static APPLICATION_JSON = "application/json";
-    static APPLICATION_PDF = "application/pdf";
-    static APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
-    static APPLICATION_OCTET_STREAM = "application/octet-stream";
-    static APPLICATION_MS_EXCEL = "application/vnd.ms-excel";
-    static MULTIPART_FORM_DATA = "multipart/form-data";
-    static TEXT_PLAIN = "text/plain";
-    static TEXT_CSV = "text/csv";
-    static TEXT_XML = "text/xml";
-    static TEXT_HTML = "text/html";
-    static TEXT_CSS = "text/css";
-    static TEXT_JAVASCRIPT = "text/javascript";
-    static TEXT_CACHE_MANIFEST = "text/cache-manifest";
-    static IMAGE_PNG = "image/png";
-    static IMAGE_GIF = "image/gif";
-    static IMAGE_X_ICON = "image/x-icon";
-    static IMAGE_JPEG = "image/jpeg";
-    static IMAGE_SVG = "image/svg+xml";
-    static FONT_WOFF = "application/x-font-woff";
-    static FONT_SFNT = " application/font-sfnt";
 }
