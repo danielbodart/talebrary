3de5bda2a7ce72c85786d182937f94f1be468982
Daniel Bodart <dan@bodar.com>
2024-05-26 17:49:45 +0100

    Add ability to detect playable game types

--- src/D1GameFinder.ts
+++ src/D1GameFinder.ts
@@ -7,7 +7,7 @@ export interface GameInfo {
     author: string;
     description: string;
     coverart: string;
-    url: string;
+    playable: boolean;
 }
 
 export class D1GameFinder {
@@ -16,15 +16,19 @@ export class D1GameFinder {
 
     async find(search: string): Promise<GameInfo[]> {
         const sql = `
-select g.id, avg(r.rating) as rating, g.title, g.author, g.desc as description, g.coverart, l.url
-from games g
-         join reviews r on g.id = r.gameid
-         join gamelinks l on g.id = l.gameid
-where g.title like ? 
-  and l.displayorder = 0
-and g.coverart is not null
-group by 1
-order by 2 desc;`;
+            select g.id, avg(r.rating) as rating, g.title, g.author, g.desc as description, g.coverart,
+                   (select
+                        CASE WHEN count(*) > 0 THEN 1 ELSE 0 END
+                    from gamelinks l
+                             join filetypes f on l.fmtid = f.id
+                    where l.gameid = g.id
+                      and f.externid IN ('zcode', 'blorb/zcode', 'glulx', 'blorb/glulx', 'hugo', 'adrift', 'tads2', 'tads3')) as playable
+            from games g
+                     join reviews r on g.id = r.gameid
+            where g.title like ?
+              and g.coverart is not null
+            group by 1
+            order by 2 desc;`;
         const statement = this.db.prepare(sql).bind(`%${search}%`);
         return (await statement.all()).results as any;
     }
