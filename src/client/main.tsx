1734d111420bb2ae92b2c8e8e6c5b083038efc00
Daniel Bodart <dan@bodar.com>
2024-05-30 22:31:14 +0100

    Add icons for prompt

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -2,7 +2,7 @@ import {client, WindowMessageHandler} from "./client.ts";
 import type {SupportedGameType} from "../types.ts";
 import type {BufferContent, UpdateMessage} from "./types.ts";
 import * as elements from "typed-html";
-import {fragment} from "../templates/Fragment.tsx";
+import {Fragment, fragment} from "../templates/Fragment.tsx";
 
 (async () => {
     const story = document.querySelector<HTMLLinkElement>('#story');
@@ -25,21 +25,31 @@ const breakOn = new Set(['header', 'subheader']);
 export function render(update: UpdateMessage): string {
     const bufferId = update.windows.find(w => w.type === 'buffer')?.id;
     const bufferContent = update.content.filter(c => c.id === bufferId) as BufferContent[];
-    return <div class="card">
-        {bufferContent.flatMap(b => {
-            return b.text.flatMap(t => {
-                if (!t.content) {
-                    return <div class="softbreak"></div>;
-                } else if (t.content.length === 1) {
-                    return t.content.map(c => {
-                        return (breakOn.has(c.style) ? '</div><div class="card">' : '') +
-                            <div class={c.style}>{c.text}</div>;
-                    });
-                } else {
-                    return <div class="paragraph">{t.content.map(c => <span class={c.style}>{c.text}</span>)}</div>
-                }
-            });
-        }).join('')}
-    </div>
+    const input = update.input.find(c => c.id === bufferId);
+    return <Fragment>
+        <div class="card">
+            {bufferContent.flatMap(b => {
+                return b.text.flatMap(t => {
+                    if (!t.content) {
+                        return <div class="softbreak"></div>;
+                    } else if (t.content.length === 1) {
+                        return t.content.map(c => {
+                            return (breakOn.has(c.style) ? '</div><div class="card">' : '') +
+                                (c.text === '>' ? '' : <div class={c.style}>{c.text}</div>)
+                        });
+                    } else {
+                        return <div class="paragraph">{t.content.map(c => <span class={c.style}>{c.text}</span>)}</div>
+                    }
+                });
+            }).join('')}
+        </div>
+        {input ?
+            <div class="card">
+                <div class="input">
+                    <input type="text" maxlength={String(input.maxlen ?? 256)} autofocus="autofocus"/>
+                </div>
+            </div>
+            : ''}
+    </Fragment>
 }
 
