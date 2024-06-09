7fe7dbb4b1f8e21ff87844f083de6cf3ec7d6924
Daniel Bodart <dan@bodar.com>
2024-06-09 08:32:50 +0100

    Switch to JSON model

--- src/content/ArtHandler.ts
+++ src/content/ArtHandler.ts
@@ -7,14 +7,21 @@ export class ArtHandler {
 
     async handle(request: Request): Promise<Response> {
         const {query} = new Uri(request.url);
-        const prompt = new URLSearchParams(query).get('prompt') ?? "cyberpunk cat";
+        const params = new URLSearchParams(query);
 
-        const response = await this.ai.run(
-            "@cf/bytedance/stable-diffusion-xl-lightning",
-            {
-                prompt,
-            }
-        );
+        const rawPrompt = params.get('prompt');
+        if (!rawPrompt) return new Response('Not Found', {status: 404});
+
+        const model = params.get('model') ?? "@cf/bytedance/stable-diffusion-xl-lightning" as any;
+        const data = JSON.parse(rawPrompt);
+        const prompt = `
+                    You are an illustrator for the interactive fiction story called ${data.story.title} by ${data.story.author}, ${data.story.description ? `which is described as ${data.story.description}` : ''}.
+                    You need to create an image for the current scene who's title is ${data.scene.title} and described as ${data.scene.description}.
+                    The image should have a strong connection with the scene title.
+                    
+                    `.replace(/\s+/g, ' ');
+
+        const response = await this.ai.run(model, {prompt});
 
         return new Response(response, {
             headers: {
