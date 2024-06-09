58ff9197f11860c0dd76dd2e6a57cda045cbb846
Daniel Bodart <dan@bodar.com>
2024-06-09 09:56:39 +0100

    Fix previous

--- src/content/ArtHandler.ts
+++ src/content/ArtHandler.ts
@@ -18,10 +18,9 @@ export class ArtHandler {
                     Create an illustration for the interactive fiction story "${data.story.title}" by "${data.story.author}". 
                     The scene is titled "${data.scene.title}" and is described as follows 
                     "${data.scene.description.replace('"', '`')}"
-                    The scene should be consistent with the previous scene which was titled "${data.previous.title}" 
-                    and was described as follows "${data.previous.description.replace('"', '`')}"
-
-                    `.replace(/\s+/g, ' ');
+                    ${data.previous ? `The scene should be consistent with the previous scene which was titled "${data.previous.title}" 
+                    and was described as follows "${data.previous.description.replace('"', '`')}"` : ''
+        }`.replace(/\s+/g, ' ');
 
         const response = await this.ai.run(model, {prompt});
 
