506db6690a0af1843fba653c49ac29a6bce825d6
Daniel Bodart <dan@bodar.com>
2024-06-22 12:31:38 +0100

    Improve which story file it uses

--- test/D1GameFinder.test.ts
+++ test/D1GameFinder.test.ts
@@ -35,4 +35,17 @@ describe("D1GameFinder", () => {
             type: "zcode",
         } as any);
     });
+
+    test("ignores zip files and instead returns links to raw game file", async () => {
+        const finder = new D1GameFinder(db);
+        const result = await finder.get('qpecxgjpxnvw50xq');
+        expect(result).toEqual( {
+            id: "qpecxgjpxnvw50xq",
+            title: "The Elysium Enigma",
+            author: "Eric Eve",
+            description: "It was meant to be a routine visit on behalf of the imperial government, just to remind the settlers that the Empire hadn't forgotten them, and if you stick rigidly to the letter of your orders and refuse to use either your eyes or your initiative that's all it'll be. But with an interstellar war brewing in the background, you'd be wise not to take anything for granted.",
+            url: "http://www.ifarchive.org/if-archive/games/competition2006/tads3/elysium/Elysium.t3",
+            type: "tads3",
+        } as any);
+    });
 })
